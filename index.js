3199f6562cd3f6cdfbea25e25b27b37eca645608
Tobias Murphy <murphy.tobias@gmail.com>
2017-01-30 10:04:05 -0800

    hooked up css, js, font, populated index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+var express = require('express');
+var ejsLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
+var request = require('request');
+var session = require('express-session');
+var passport = require('./config/ppConfig');
+var flash = require('connect-flash');
+var isLoggedIn = require('./middleware/isLoggedIn');
+var app = express();
+var db = require("./models");
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/*', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
+app.set('view engine', 'ejs');
+
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'supersecretpassword',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// app.use(flash());
+//
+// app.use(function(req, res, next) {
+//     res.locals.alerts = req.flash();
+//     res.locals.currentUser = req.user;
+//     next();
+// });
+
+});
+
+//auth here
+
+
+var server = app.listen(process.env.PORT || 3000);
+
+module.exports = server;
