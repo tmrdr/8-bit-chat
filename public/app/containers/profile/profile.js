094e0dfe5c0e5cb7fdbb08c7891d3eb3c4bbb98b
Tobias Murphy <murphy.tobias@gmail.com>
2017-01-30 14:15:22 -0800

    added profile page

---
+++ public/app/containers/profile/profile.js
@@ -0,0 +1,12 @@
+angular.module('ChatApp')
+.component('profileComp', {
+  templateUrl: 'app/containers/profile/profile.html',
+  controller: ProfileCompCtrl,
+  controllerAs: 'ProfileComp'
+});
+
+function ProfileCompCtrl() {
+
+}
+
+ProfileCompCtrl.$inject = [];
