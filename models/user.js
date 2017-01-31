da12751049d89a9c76c548b8856ac10b615dd754
Robert1GA <roberts.2015@yahoo.co.uk>
2017-01-31 11:19:48 -0800

    re-added models-user.js, re-added empty services.js

--- models/user.js
+++ models/user.js
@@ -1,14 +1,41 @@
-'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var user = sequelize.define('user', {
-    name: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
-  return user;
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+var UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+UserSchema.set('toJSON', {
+  transform: function(doc, ret, options) {
+    var returnJson = {
+      id: ret._id,
+      name: ret.name
+    };
+    return returnJson;
+  }
+});
+
+UserSchema.methods.authenticated = function(password) {
+  var user = this;
+  var isAuthenticated = bcrypt.compareSync(password, user.password);
+  return isAuthenticated ? user : false;
 };
+
+UserSchema.pre('save', function(next) {
+  if (!this.isModified('password')) {
+    next();
+  } else {
+    this.password = bcrypt.hashSync(this.password, 10);
+    next();
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
