6b1c7fecf98ccf168b12236cd6089bfd2e4e2b4e
Robert1GA <roberts.2015@yahoo.co.uk>
2017-01-31 11:15:59 -0800

    repairing auth merge with new page structure. index.js good to go

---
+++ controllers/users.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var User = require('../models/user'); // ???
+var router = express.Router();
+
+router.route('/')
+  .get(function(req, res) {
+    User.find(function(err, users) {
+      if (err) return res.status(500).send(err);
+
+      return res.send(users);
+    });
+  })
+  .post(function(req, res) {
+    // find the user first in case the email already exists
+    User.findOne({ email: req.body.email }, function(err, user) {
+      if (user) return res.status(400).send({ message: 'Email already exists' });
+
+      User.create(req.body, function(err, user) {
+        if (err) return res.status(500).send(err);
+
+        return res.send(user);
+      });
+    });
+  });
+
+router.get('/:id', function(req, res) {
+  User.findById(req.params.id, function(err, user) {
+    if (err) return res.status(500).send(err);
+
+    return res.send(user);
+  });
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -5,6 +5,16 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var session = require('express-session');
 var flash = require('connect-flash');
+
+// JSON web token dependencies, including a secret key to sign the token
+var expressJWT = require('express-jwt');
+var jwt = require('jsonwebtoken');
+var secret = process.env.JWT_SECRET;
+
+// mongoose models and connection
+var mongoose = require('mongoose');
+var User = require('./models/user');
+
 // var isLoggedIn = require('./middleware/isLoggedIn');
 var app = express();
 // var db = require("./models");
@@ -14,10 +24,40 @@ var io = require('socket.io')(http);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// AUTH HERE
+app.use('/api/users', expressJWT({secret: secret}).unless({
+  path: [{ url: '/api/users', methods: ['POST'] }]
+}), require('./controllers/users'));
+
+// this middleware will check if expressJWT did not authorize the user, and return a message
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send({ message: 'You need an authorization token to view this information.' });
+  }
+});
+
+// POST /api/auth - if authenticated, return a signed JWT
+app.post('/api/auth', function(req, res) {
+  User.findOne({ name: req.body.name }, function(err, user) {  // EMAIL BECOMES NAME ??  also DB
+    // return 401 if error or no user
+    if (err || !user) return res.status(401).send({ message: 'User not found' });
+
+    // attempt to authenticate a user
+    var isAuthenticated = user.authenticated(req.body.password);
+    // return 401 if invalid password or error
+    if (err || !isAuthenticated) return res.status(401).send({ message: 'User not authenticated' });
+
+    // sign the JWT with the user payload and secret, then return
+    var token = jwt.sign(user.toJSON(), secret);
+
+    return res.send({ user: user, token: token });
+  });
+});
+
+
 app.get('/*', function(req, res){
   res.sendFile(path.join(__dirname, 'public/index.html'));
 })
-
 
 //socket
 io.on('connection', function(socket){
@@ -63,8 +103,7 @@ io.on('connection', function(socket){
 });
 
 
-
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({
@@ -72,11 +111,11 @@ app.use(bodyParser.urlencoded({
 }));
 
 
-app.use(session({
-    secret: process.env.SESSION_SECRET || 'supersecretpassword',
-    resave: false,
-    saveUninitialized: true
-}));
+// app.use(session({
+//     secret: process.env.SESSION_SECRET || 'supersecretpassword',
+//     resave: false,
+//     saveUninitialized: true
+// }));
 
 
 // app.use(flash());
@@ -89,10 +128,9 @@ app.use(session({
 
 // });
 
-//auth here
 
-http.listen(3000);
+// http.listen(3000);
 
 // var server = app.listen(process.env.PORT || 3000);
-//
-// module.exports = server;
+var server = http.listen(process.env.PORT || 3000)
+module.exports = server;
