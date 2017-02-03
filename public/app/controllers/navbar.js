32efa190ccc47d92f3991191f3ab0edc13871cdf
skylocke <mathematical.fiction@gmail.com>
2017-02-02 20:49:14 -0800

    minor fix with navbar profile pic

--- public/app/controllers/navbar.js
+++ public/app/controllers/navbar.js
@@ -2,12 +2,13 @@ angular.module('ChatApp')
 .controller('NavCtrl', ['$scope', 'Auth', '$state', 'Alerts', 'UserService', function($scope, Auth, $state, Alerts, UserService) {
   $scope.Auth = Auth;
 
-  UserService.getColors().then(function success(res) {
-    $scope.userSettings = res.data;
-  },function error(res) {
-    console.log(res);
-  });
-
+  if(Auth.currentUser()) {
+    UserService.getColors().then(function success(res) {
+      $scope.userSettings = res.data;
+    },function error(res) {
+      console.log(res);
+    });
+  }
   $scope.logout = function() {
     Auth.removeToken();
     Alerts.add('success', 'Logged out!');
